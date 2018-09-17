e9e8d7cde5447525bd6b9cb1ca014997582944cd
yashdeep97 <yashdeep97@gmail.com>
2018-09-18 01:50:51 +0530

    remove excess file

--- ScrapedData/mycsv.py
+++ ScrapedData/mycsv.py
@@ -16,8 +16,8 @@ import numpy
 #Importing the Cosine Score Class for calculating the ranking
 from CosineScore import *
 
-reload(sys)
-sys.setdefaultencoding('utf8')
+# reload(sys)
+# sys.setdefaultencoding('utf8')
 
 ###################### DATA Pre-Processing #########################
 
