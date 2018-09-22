045416e6bebddd269f3cdc9db7ca8ad63ee10920
yashdeep97 <yashdeep97@gmail.com>
2018-09-23 01:36:33 +0530

    add boostrap and css for search bar

--- ScrapedData/gui.py
+++ ScrapedData/gui.py
@@ -8,20 +8,54 @@ from dash.dependencies import Input,Output,State
 from SearchQuery import SearchQuery
 
 ####################### GLOBAL VARIABLES #################################
-app = dash.Dash()
+
+external_scripts = [
+    {
+        'src' : "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+        'integrity' : "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo",
+        'crossorigin' : "anonymous"
+    },
+    {
+        'src' : "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
+        'integrity' : "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49",
+        'crossorigin' : "anonymous"
+    },
+    {
+        'src' : "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js",
+        'integrity' : "sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy",
+        'crossorigin' : "anonymous"
+    }
+]
+
+external_stylesheets = [
+    {
+        'href' : 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'rel' : 'stylesheet',
+        'integrity' : 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'crossorigin' : 'anonymous'
+    }
+]
+
+app = dash.Dash(__name__, external_scripts=external_scripts, external_stylesheets=external_stylesheets)
 
 ####################### DASH DESIGNING ###################################
-app.layout=html.Div(children=[
+
+app.layout = html.Div(children=[
     ######### SEARCH BAR DESIGN ########
-    html.Div(children=[
-        ### Ading input box for query input
-        dcc.Input(id='search_box',value='Andy',type='text',style={'width':'70%'}),
-        ### Adding the search button
-        html.Button(id='search_button',children='Search',n_clicks=0,style={'width':'20%','margin-left':'5px'})
-    ],
-    style={'width':'100%',
-            'display':'inline-block'}
-    ),
+    html.Nav(className='navbar navbar-dark', children=[
+        html.Div(className='container-fluid', children=[
+            html.Div(id='search_box_container', className='row justify-content-md-center', children=[
+                html.Div(className='input-group col-9', children=[
+                    ### Ading input box for query input
+                    dcc.Input(id='search_box', className='form-control', value='',placeholder='Enter Text',type='text'),
+                    html.Div(className='input-group-append', children=[
+                        ### Adding the search button
+                        html.Button(id='search_button', className='btn btn-primary',children='Search',n_clicks=0)
+                    ])
+                ])
+            ])
+        ])
+    ]),
 
     ######## RESULT DISPLAY ###########
     html.Div(children=[
